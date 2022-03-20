803abc0d8fe86c086285c1f37ad5b59c9ccffcf5
estheradwets <estheradwets00@gmail.com>
2022-03-20 19:22:44 +0300

    Add ptototype and Constructor

--- index.js
+++ index.js
@@ -1,209 +1,174 @@
 
-// $("document").ready(function(){
-//     alert("This works")
-// });
-
-(function($) {
-    $('.summary').hide();
-    $('.cdata-overlay').hide();
-    $('.deliver').hide();
-    $('.delivernot').hide();
-
+/* $("document").ready(function(){
+    alert("This works")
 });
+ */
 
- //Get inputs
- $("#checkout").click(function () {
-    let flavour =$("#flavour option:selected").val();
-    let size = $("#psize option:selected").val();
-    let crust = $("#pcrust option:selected").val();
-    let topping = $("#ptopping option:selected").val();
-    let number = $("#count").val();
-    console.log(topping);
 
-    //Function order
-    let order = (f,s, c, t, n, total) => {
-        return {f,s, c, t, n, total};
-    };
+ //View inputs
 
- });
 
-/* // THE FORMULA
-// checkout price
-        let price, totalPrice;
-        switch (flavour) {
-            case flavour = "Chicken Hawaian":
-                switch (size) {
-                    case size = "SMALL":
-                        price = 500;
-                        if (crust === "CRISPY") {
-                            totalPrice = (price * number) + 100;
-                        } else if (crust === "STUFFED") {
-                            totalPrice = (price * number) + 150;
-                        } else if (crust === "GLUTTEN-FREE") {
-                            totalPrice = (price * number) + 200;
-                        } else {
-                            totalPrice = (price * number) + 300;
-                        }
-                        break;
-                        case size = "MEDIUM":
-                            price = 800;
-                            if (crust === "CRISPY") {
-                                totalPrice = (price * number) + 100;
-                            } else if (crust === "STUFFED") {
-                                totalPrice = (price * number) + 150;
-                            } else if (crust === "GLUTTEN-FREE") {
-                                totalPrice = (price * number) + 200;
-                            } else {
-                                totalPrice = (price * number) + 300;
-                            }
-                            break;
-                            case size = "LARGE":
-                                price = 1200;
-                                if (crust === "CRUSTY") {
-                                    totalPrice = (price * number) + 100;
-                                } else if (crust === "STUFFED") {
-                                    totalPrice = (price * number) + 150;
-                                } else if (crust === "GLUTTEN-FREE") {
-                                    totalPrice = (price * number) + 200;
-                                } else {
-                                    totalPrice = (price * number) + 300;
-                                }
-                                break;
-                        }
-                        break;
-            case flavour = "Veg Feast":
-                switch (size) {
-                    case size = "SMALL":
-                        price = 500;
-                        if (crust === "CRISPY") {
-                            totalPrice = (price * number) + 100;
-                        } else if (crust === "STUFFED") {
-                            totalPrice = (price * number) + 150;
-                        } else if (crust === "GLUTTEN-FREE") {
-                            totalPrice = (price * number) + 200;
-                        } else {
-                            totalPrice = (price * number) + 300;
-                        }
-                        break;
-                    case size = "MEDIUM":
-                        price = 800;
-                        if (crust === "thin") {
-                            totalPrice = (price * number) + 100;
-                        } else if (crust === "thick") {
-                            totalPrice = (price * number) + 150;
-                        } else if (crust === "flatbread") {
-                            totalPrice = (price * number) + 200;
-                        } else {
-                            totalPrice = (price * number) + 300;
-                        }
-                        break;
-                    case size = "LARGE":
-                        price = 1200;
-                        if (crust === "thin") {
-                            totalPrice = (price * number) + 100;
-                        } else if (crust === "thick") {
-                            totalPrice = (price * number) + 150;
-                        } else if (crust === "flatbread") {
-                            totalPrice = (price * number) + 200;
-                        } else {
-                            totalPrice = (price * number) + 300;
-                        }
-                        break;
-                }
-                break;
-            case flavour = "Meat Deluxe":
-                switch (size) {
-                    case size = "SMALL":
-                        price = 500;
-                        if (crust === "CRISPY") {
-                            totalPrice = (price * number) + 100;
-                        } else if (crust === "STUFFED") {
-                            totalPrice = (price * number) + 150;
-                        } else if (crust === "GLUTTEN-FREE") {
-                            totalPrice = (price * number) + 200;
-                        } else {
-                            totalPrice = (price * number) + 300;
-                        }
-                        break;
-                    case size = "MEDIUM":
-                        price = 800;
-                        if (crust === "CRISPY") {
-                            totalPrice = (price * number) + 100;
-                        } else if (crust === "STUFFED") {
-                            totalPrice = (price * number) + 150;
-                        } else if (crust === "GLUTTEN-FREE") {
-                            totalPrice = (price * number) + 200;
-                        } else {
-                            totalPrice = (price * number) + 300;
-                        }
-                        break;
-                    case size = "LARGE":
-                        price = 1200;
-                        if (crust === "thin") {
-                            totalPrice = (price * number) + 100;
-                        } else if (crust === "thick") {
-                            totalPrice = (price * number) + 150;
-                        } else if (crust === "flatbread") {
-                            totalPrice = (price * number) + 200;
-                        } else {
-                            totalPrice = (price * number) + 300;
-                        }
-                        break;
-                }
-                break;
-            }
-            
-        switch (topping) {
-            case topping = "Roasted Brocolli":
-                totalPrice = totalPrice + 100;
-                break;
-            case topping = "Roasted Fennel":
-                totalPrice = totalPrice + 100;
-                break;
-            case topping = "Extra Cheese":
-                totalPrice = totalPrice + 100;
-                break;
-            
 
+//  BUILDING CONSTRUCTOR
+
+  function Pizzeria(flavour,size,crust,topping,){
+      this.flavour = flavour
+      this.size = size
+      this.crust = crust
+      this.topping = topping
+      
+  }
+    //   BUILDING PROTOTYPE
+
+
+    Pizzeria.prototype.totalcost = function(){
+        let flavourprice;
+        let sizeprice;
+        let crustprice;
+        let toppingprice;  
+        
+
+        // FOR FLAVOUR
+
+        if (this.flavour==="chicken"){
+            flavourprice = 1000;
         }
-        //Execute order function
-        let newOrder = order(flavour, size, crust, topping, number, totalPrice);
-        console.log(newOrder); 
+    
+        else if (this.flavour==="vegan"){
+            flavourprice = 800
+        }
+        else{
+            flavourprice = 500
+        }
 
-        //Write to the order
-        $('.summary').slideDown(2000);
-        $('.cdata-overlay').slideUp();
-        $('#list').slideDown();
-        $('.deliver').show(1000);
-        $('.delivernot').show(1000);
+      console.log(flavourprice)
+
+    // FOR SIZE
+
+        if (this.size==="larger"){
+            sizeprice = 100;
+        }
+        else if (this.size==="regular"){
+            sizeprice = 50
+        }
+        else{
+            sizeprice = 10
+        }
+        console.log(sizeprice)
+
+        // FOR CRUST
+          
+        if( this.crust==="thick"){
+            crustprice = 150;
+        }
+        else if(this.crust ==="thin"){
+            crustprice = 100
+        }
+        else{
+           crustprice = 10
+        }
+        console.log(crustprice)
+
+        // FOR TOPPING
+  
+        if( this.topping==="best"){
+            toppingprice = 100;
+        }
+        else if(this.topping ==="better"){
+            toppingprice = 50
+        }
+        else{
+           toppingprice = 10
+        }
+        console.log(toppingprice)
+
+        let totalprice = (flavourprice + sizeprice + crustprice + toppingprice)*parseInt(quantity)
+        console.log(totalprice)
+        // let total = parseInt(quantity)*totalprice
+        // console.log(total)
+        return totalprice
+        
+    };
+    
+    $("#checkout").click(function(){
+        let flavour =$("#flavour option:selected").val();
+        let size = $("#psize option:selected").val();
+        let crust = $("#pcrust option:selected").val();
+        let topping = $("#ptopping option:selected").val();
+         let newOrder = new Pizzeria(flavour,size,crust,topping)
+    
+         console.log(newOrder.totalcost())
+    
+     });
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     /*  $(".table").hide();
+      $(".additional-buttons").hide();
+      $(".additional-info").hide();
+      $(".btn.yes").hide();
+      $(".btn.no").hide();
+      $(".additional-info h4").hide();
+
+      $('.btn.order').click(function() {
+        var sizeOfPizza = $("#psize option:selected").val();
+        var toppingsOfPizza = $("#ptopping option:selected").val();
+        var crustOfPizza = $("#pcrust option:selected").val();
+        var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+        var order = 1;
+        var grandTotal = 0;
+    
+        $(".table").show();
+        $(".additional-buttons").show();
+        $(".btn.order").hide();
+    
+        $("#size").html($(".size option:selected").text() + " - " + sizeOfPizza);
+        $("#toppings").html($(".toppings option:selected").text() + " - " + toppingsOfPizza);
+        $("#crust").html($(".crust option:selected").text() + " - " + crustOfPizza);
+        $("#total").html(total);
 
     
 
-        $('#list').text(" ")
-        $("#list").append("<br>" + "Flavour Chosen is:   " + newOrder.f + "<br>" + "Size :   "
-            + newOrder.s + "<br>" + "Crust Chosen is:     "
-            + newOrder.c + "<br>" + "Toppings Chosen:     "
-            + newOrder.t + "<br>" + " Number of pizzas :    "
-            + newOrder.n + "<br>" + "The Total Price is KSH:  "
-            + newOrder.total + "<br><br>").css('font-size', '30px');
-    });
 
-       //Deliver
-       $(".deliver").click(function () {
-        $('.summary').slideUp();
-        $('#list').slideUp();
-        $('.summary').text("Provide location details").slideDown();
-        $('.deliver').hide(1000);
-        $('.delivernot').hide(1000);
-        $('.cdata-overlay').slideDown();
-    });
  */
-    $("#submit").click(function(e){
-        e.preventDefault()
-        var name=$("#jina").val()
-        var email=$("#email").val()
-       var message=$("#message").val()
 
-      alert(`${name} Your message has been submitted`)
-      });
-      
+
+
+
+
+
+
+
+
+
+ /* $("#submit").click(function(e){
+    e.preventDefault()
+    var name=$("#jina").val()
+    var email=$("#email").val()
+   var message=$("#message").val()
+
+  alert(`${name} Your message has been submitted`)
+  });
+   */
