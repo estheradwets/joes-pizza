00f09701a68201c34ae1a381be3a04814c7425e9
estheradwets <estheradwets00@gmail.com>
2022-03-20 12:30:22 +0300

    Add formular

--- index.js
+++ index.js
@@ -20,8 +20,52 @@
     console.log(topping);
 
     //Function order
-    let order = (s, c, t, n, total) => {
-        return {s, c, t, n, total};
+    let order = (f,s, c, t, n, total) => {
+        return {f,s, c, t, n, total};
     };
 
  });
+
+// THE FORMULA
+// checkout price
+        let price, totalPrice;
+        switch (flavour) {
+            case flavour = "Chicken Hawaian":
+                switch (size) {
+                    case size = "SMALL":
+                        price = 500;
+                        if (crust === "CRISPY") {
+                            totalPrice = (price * number) + 120;
+                        } else if (crust === "STUFFED") {
+                            totalPrice = (price * number) + 150;
+                        } else if (crust === "GLUTTEN-FREE") {
+                            totalPrice = (price * number) + 200;
+                        } else {
+                            totalPrice = (price * number) + 220;
+                        }
+                        break;
+                        case size = "MEDIUM":
+                            price = 800;
+                            if (crust === "CRISPY") {
+                                totalPrice = (price * number) + 120;
+                            } else if (crust === "STUFFED") {
+                                totalPrice = (price * number) + 150;
+                            } else if (crust === "GLUTTEN-FREE") {
+                                totalPrice = (price * number) + 200;
+                            } else {
+                                totalPrice = (price * number) + 220;
+                            }
+                            break;
+                            case size = "LARGE":
+                                price = 1200;
+                                if (crust === "CRUSTY") {
+                                    totalPrice = (price * number) + 120;
+                                } else if (crust === "STUFFED") {
+                                    totalPrice = (price * number) + 150;
+                                } else if (crust === "GLUTTEN-FREE") {
+                                    totalPrice = (price * number) + 200;
+                                } else {
+                                    totalPrice = (price * number) + 220;
+                                }
+                                break;
+                        }
