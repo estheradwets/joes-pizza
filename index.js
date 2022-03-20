1eee18eda54f87fe1f9b526b579266d1dd3c4e21
estheradwets <estheradwets00@gmail.com>
2022-03-20 12:00:17 +0300

    Add JS and checkout click behaviour

--- index.js
+++ index.js
@@ -3,3 +3,25 @@
 //     alert("This works")
 // });
 
+(function($) {
+    $('.summary').hide();
+    $('.cdata-overlay').hide();
+    $('.deliver').hide();
+    $('.delivernot').hide();
+
+});
+
+ //Get inputs
+ $("#finish").click(function () {
+    let size = $("#psize option:selected").val();
+    let crust = $("#pcrust option:selected").val();
+    let topping = $("#ptopping option:selected").val();
+    let number = $("#count").val();
+    console.log(topping);
+
+    //Function order
+    let order = (s, c, t, n, total) => {
+        return {s, c, t, n, total};
+    };
+
+ });
